f40c6d6803135000b9bcf4e2483c8afb184eea59
isabella g henriques <isabellagh88@gmail.com>
2020-06-22 01:40:01 +0000

    add bft

---
+++ lib/bmore_foodtrucks.rb
@@ -0,0 +1,6 @@
+require "bmore_foodtrucks/version"
+
+module BmoreFoodtrucks
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bmore_foodtrucks/version.rb
@@ -0,0 +1,3 @@
+module BmoreFoodtrucks
+  VERSION = "0.1.0"
+end
